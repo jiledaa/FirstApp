dafc7646b15ebd4ef86a80d8ea0175b8668a1597
David Jilek <jiledaa@icloud.com>
2021-11-08 16:33:46 +0100

    HIIT app until page 76

---
+++ HIITFit/View/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+          WelcomeView()
+            ForEach(0 ..< 4) { index in
+                ExerciseView(index: index)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+    }
+}
+
+struct Exercise_2_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
