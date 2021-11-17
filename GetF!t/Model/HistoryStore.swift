cce0fc69020615eed4304aae20fce9b7d658dd8b
David Jilek <jiledaa@icloud.com>
2021-11-17 17:38:59 +0100

    GetF!t until page 220

--- GetF!t/Model/HistoryStore.swift
+++ GetF!t/Model/HistoryStore.swift
@@ -9,15 +9,77 @@ struct ExerciseDay: Identifiable {
 class HistoryStore: ObservableObject {
   @Published var exerciseDays: [ExerciseDay] = []
 
+    enum FileError: Error {
+      case loadFailure
+      case saveFailure
+      case urlFailure
+    }
+
   init() {
     #if DEBUG
-    createDevData()
+//    createDevData()
     #endif
   }
 
+    func getURL() -> URL? {
+      // 1
+      guard let documentsURL = FileManager.default.urls(
+        for: .documentDirectory, in: .userDomainMask).first else {
+        // 2
+        return nil
+      }
+      // 3
+      return documentsURL.appendingPathComponent("history.plist")
+    }
+
+    func load() throws {
+      // 1
+      guard let dataURL = getURL() else {
+        throw FileError.urlFailure
+      }
+      do {
+        // 2
+        let data = try Data(contentsOf: dataURL)
+        // 3
+        let plistData = try PropertyListSerialization.propertyList(
+          from: data,
+          options: [],
+          format: nil)
+        // 4
+        let convertedPlistData = plistData as? [[Any]] ?? []
+        // 5
+        exerciseDays = convertedPlistData.map {
+          ExerciseDay(
+            date: $0[1] as? Date ?? Date(),
+            exercises: $0[2] as? [String] ?? [])
+        }
+      } catch {
+        throw FileError.loadFailure
+      }
+    }
+
+    func save() throws {
+      guard let dataURL = getURL() else {
+        throw FileError.urlFailure
+      }
+      let plistData = exerciseDays.map {
+        [$0.id.uuidString, $0.date, $0.exercises]
+      }
+      do {
+        let data = try PropertyListSerialization.data(
+          fromPropertyList: plistData,
+          format: .binary,
+          options: .zero)
+        try data.write(to: dataURL, options: .atomic)
+      } catch {
+        throw FileError.saveFailure
+      }
+    }
+
   func addDoneExercise(_ exerciseName: String) {
     let today = Date()
-    if today.isSameDay(as: exerciseDays[0].date) {
+      if let firstDate = exerciseDays.first?.date,
+         today.isSameDay(as: firstDate) {
       print("Adding \(exerciseName)")
       exerciseDays[0].exercises.append(exerciseName)
     } else {
@@ -25,6 +87,11 @@ class HistoryStore: ObservableObject {
         ExerciseDay(date: today, exercises: [exerciseName]),
         at: 0)
     }
+      do {
+          try save()
+      } catch {
+          fatalError(error.localizedDescription)
+      }
   }
 }
 
