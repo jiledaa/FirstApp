d9428e177d6a1ee89a37e9dcf270fcb0721da4a5
Alžbeta Gogoláková <alzbeta.gogolakova@quanti.cz>
2022-01-04 23:11:47 +0100

    REF: removes unnecessary \' characters

--- GetF!t/SupportingFiles/LocalizedStringProvider.swift
+++ GetF!t/SupportingFiles/LocalizedStringProvider.swift
@@ -2,37 +2,37 @@ import SwiftUI
 
 struct LocalizedStringProvider {
     struct ExercisesNames {
-        static let `burpee` = LocalizedStringKey("Burpee")
-        static let `squat` = LocalizedStringKey("Squat")
-        static let `stepUp` = LocalizedStringKey("StepUp")
-        static let `sunSalute` = LocalizedStringKey("SunSalute")
+        static let burpee = LocalizedStringKey("Burpee")
+        static let squat = LocalizedStringKey("Squat")
+        static let stepUp = LocalizedStringKey("StepUp")
+        static let sunSalute = LocalizedStringKey("SunSalute")
     }
 
     struct Button {
-        static let `getStarted` = LocalizedStringKey("GetStarted")
-        static let `startExercise` = LocalizedStringKey("StartExercise")
-        static let `done` = LocalizedStringKey("Done")
-        static let `history` = LocalizedStringKey("History")
+        static let getStarted = LocalizedStringKey("GetStarted")
+        static let startExercise = LocalizedStringKey("StartExercise")
+        static let done = LocalizedStringKey("Done")
+        static let history = LocalizedStringKey("History")
         static let `continue` = LocalizedStringKey("Continue")
     }
 
     struct WelcomePage {
-        static let `welcome` = LocalizedStringKey("Welcome")
-        static let `getfit` = LocalizedStringKey("Getf!t")
-        static let `getfitPostScript` = LocalizedStringKey("GetfitPostScript")
-        static let `history` = LocalizedStringKey("History")
+        static let welcome = LocalizedStringKey("Welcome")
+        static let getfit = LocalizedStringKey("Getf!t")
+        static let getfitPostScript = LocalizedStringKey("GetfitPostScript")
+        static let history = LocalizedStringKey("History")
         static let `continue` = LocalizedStringKey("Continue")
     }
 
     struct SuccesPage {
-        static let `success` = LocalizedStringKey("Success")
-        static let `glory` = LocalizedStringKey("Glory")
-        static let `gloryPostScript` = LocalizedStringKey("GloryPostScript")
+        static let success = LocalizedStringKey("Success")
+        static let glory = LocalizedStringKey("Glory")
+        static let gloryPostScript = LocalizedStringKey("GloryPostScript")
     }
     
     struct Errors {
-        static let `couldntFind` = LocalizedStringKey("CouldntFind")
-        static let `loadingError1` = LocalizedStringKey("LaodingError1")
-        static let `loadingError2` = LocalizedStringKey("LoadingError2")
+        static let couldntFind = LocalizedStringKey("CouldntFind")
+        static let loadingError1 = LocalizedStringKey("LaodingError1")
+        static let loadingError2 = LocalizedStringKey("LoadingError2")
     }
 }
